68fe845af8e483d53fb9aec038dd60c610efdd1c
niso120b <niso120b@gmail.com>
2020-03-25 14:30:38 +0200

    Update deployment section

--- delete.sh
+++ delete.sh
@@ -1,12 +1,13 @@
-kubectl delete -f  deploy/crds/operator_v1alpha1_aquadatabase_crd.yaml 
-kubectl delete -f  deploy/crds/operator_v1alpha1_aquagateway_crd.yaml 
-kubectl delete -f  deploy/crds/operator_v1alpha1_aquaserver_crd.yaml 
-kubectl delete -f  deploy/crds/operator_v1alpha1_aquaenforcer_crd.yaml
-kubectl delete -f  deploy/crds/operator_v1alpha1_aquacsp_crd.yaml 
-kubectl delete -f  deploy/crds/operator_v1alpha1_aquascanner_crd.yaml
+kubectl delete -f  deploy/crds/operator.aquasec.com_aquadatabases_crd.yaml 
+kubectl delete -f  deploy/crds/operator.aquasec.com_aquagateways_crd.yaml 
+kubectl delete -f  deploy/crds/operator.aquasec.com_aquaservers_crd.yaml 
+kubectl delete -f  deploy/crds/operator.aquasec.com_aquaenforcers_crd.yaml
+kubectl delete -f  deploy/crds/operator.aquasec.com_aquacsps_crd.yaml
+kubectl delete -f  deploy/crds/operator.aquasec.com_aquascanners_crd.yaml
 
 kubectl delete -f deploy/service_account.yaml -n aqua
 kubectl delete -f deploy/role.yaml
 kubectl delete -f deploy/role_binding.yaml
 kubectl delete -f deploy/operator.yaml -n aqua
 kubectl delete ns aqua
+
--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 kubectl create ns aqua
-kubectl create -f  deploy/crds/operator_v1alpha1_aquadatabase_crd.yaml 
-kubectl create -f  deploy/crds/operator_v1alpha1_aquagateway_crd.yaml 
-kubectl create -f  deploy/crds/operator_v1alpha1_aquaserver_crd.yaml 
-kubectl create -f  deploy/crds/operator_v1alpha1_aquaenforcer_crd.yaml
-kubectl create -f  deploy/crds/operator_v1alpha1_aquacsp_crd.yaml 
-kubectl create -f  deploy/crds/operator_v1alpha1_aquascanner_crd.yaml
+kubectl create -f  deploy/crds/operator.aquasec.com_aquadatabases_crd.yaml 
+kubectl create -f  deploy/crds/operator.aquasec.com_aquagateways_crd.yaml 
+kubectl create -f  deploy/crds/operator.aquasec.com_aquaservers_crd.yaml 
+kubectl create -f  deploy/crds/operator.aquasec.com_aquaenforcers_crd.yaml
+kubectl create -f  deploy/crds/operator.aquasec.com_aquacsps_crd.yaml
+kubectl create -f  deploy/crds/operator.aquasec.com_aquascanners_crd.yaml
 
 kubectl create -f deploy/service_account.yaml -n aqua
 kubectl create -f deploy/role.yaml
