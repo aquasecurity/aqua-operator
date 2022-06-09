fd4c53e949dbb51233633ebff29f0b7e1b259e9e
yossigilad-aquasec <yossi.gilad@aquasec.com>
2022-06-09 11:49:40 +0300

    Adding tests

---
+++ test/consts/testing_consts.go
@@ -0,0 +1,15 @@
+package testing_consts
+
+const (
+	Version            = "2022.4"
+	NameSpace          = "aqua"
+	CspServiceAccount  = "aqua-sa"
+	ImagePullSecret    = "aqua-registry"
+	DbDiskSize         = 10
+	DataBaseSecretKey  = "db-password"
+	DatabaseSecretName = "aqua-database-password"
+	Registry           = "registry.aquasec.com"
+	DatabaseRepo       = "database"
+	GatewayRepo        = "gateway"
+	ServerRepo         = "console"
+)
