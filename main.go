399fb3a851b791580191a53c29364113b7579996
Adi Shaull <adi.shaull@aquasec.com>
2023-06-18 12:37:43 +0300

    SLK-60435 Add cloud connect

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"github.com/aquasecurity/aqua-operator/controllers/aquasecurity/aquastarboard"
 	"github.com/aquasecurity/aqua-operator/controllers/ocp"
+	"github.com/aquasecurity/aqua-operator/controllers/operator/aquacloudconnector"
 	"github.com/aquasecurity/aqua-operator/controllers/operator/aquacsp"
 	"github.com/aquasecurity/aqua-operator/controllers/operator/aquadatabase"
 	"github.com/aquasecurity/aqua-operator/controllers/operator/aquaenforcer"
@@ -163,13 +164,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "AquaScanner")
 		os.Exit(1)
 	}
-	//if err = (&aquacloudconnector.AquaCloudConnectorReconciler{
-	//	Client: mgr.GetClient(),
-	//	Scheme: mgr.GetScheme(),
-	//}).SetupWithManager(mgr); err != nil {
-	//	setupLog.Error(err, "unable to create controller", "controller", "AquaCloudConnector")
-	//	os.Exit(1)
-	//}
+	if err = (&aquacloudconnector.AquaCloudConnectorReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "AquaCloudConnector")
+		os.Exit(1)
+	}
 
 	if err = (&aqualightning.AquaLightningReconciler{
 		Client: mgr.GetClient(),
